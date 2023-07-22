c8694dd66a794860c3293a977a016d705363436b
Daniel Jecu <catalin.jecu@gmail.com>
2023-07-22 17:10:55 +0100

    add files

--- client/src/PostList.tsx
+++ client/src/PostList.tsx
@@ -3,15 +3,15 @@ import axios from "axios";
 import CommentCreate from './CommentCreate';
 import CommentsListByPost from "./CommentsListByPost";
 
-export default () => {
-    let [posts, setPosts]= useState<Object>({});
-    const renderPosts = Object.values(posts).map( post => {
-        return (
+let PostList = () => {
+    let [posts, setPosts] = useState<Object>({});
+    const renderPosts = Object.values(posts).map(post => {
+            return (
                 <div key={post.id} className="card" style={{width: '30%', marginBottom: '20px'}}>
                     <div className="card-body">
                         <h3>{post.title}</h3>
                         <CommentsListByPost postId={post.id}/>
-                        <CommentCreate postId={post.id} />
+                        <CommentCreate postId={post.id}/>
                     </div>
                 </div>);
         }
@@ -19,14 +19,15 @@ export default () => {
 
     useEffect(() => {
         fetchPosts()
-    },[setPosts])
+    }, [setPosts])
 
-    const fetchPosts = async ()=>{
-       const res=  await axios.get("http://localhost:4000/posts");
-       setPosts(res.data);
-        console.log(posts)
+    const fetchPosts = async () => {
+        const res = await axios.get("http://localhost:4000/posts");
+        console.log("posts", res.data)
+        setPosts(res.data);
     }
     return <div className="d-flex flex-row flex-wrap justify-content-between">
         {renderPosts}
     </div>;
-}
+};
+export default PostList
--- event-bus/index.js
+++ event-bus/index.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const axios = require('axios');
+const axios = require("axios");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/events', (req, res) => {
-    const event = req.body;
-    axios.post('http://localhost:4000/events', event).catch((err) => {
-        console.log(err.message);
-    });
-    axios.post('http://localhost:4001/events', event).catch((err) => {
-        console.log(err.message);
-    });
-    axios.post('http://localhost:4002/events', event).catch((err) => {
-        console.log(err.message);
-    });
+app.post("/events", (req, res) => {
+  const event = req.body;
 
-    res.send({status:'OK'});
-})
+  console.log("event", event)
+  try {
+    axios.post("http://localhost:4000/events", event);
+    axios.post("http://localhost:4001/events", event);
+    axios.post("http://localhost:4002/events", event);
+  } catch (err) {
+    console.log(err.message);
+  }
 
-app.listen(4005, ()=>{
-    console.log('Listening on 4005');
-})
+  res.send({ status: "OK" });
+});
+
+app.listen(4005, () => {
+  console.log("Listening on 4005");
+});
